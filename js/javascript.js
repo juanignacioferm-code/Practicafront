f817628c110e186b6d7d8fdab9838c75c40dc07b
juanignacioferm-code <juanignacioferm@gmail.com>
2025-12-06 15:38:47 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+function modoAccesible() {
+    document.body.classList.toggle("accesible");
+}
+function mostrarDatos() {
+    alert("Creador del sitio: Juan Fernández\nCurso: Potrero Digital\nAño: 2025");
+}
